cbfb32a9cce1adb92947be860bf5ff2ad4a6b3fb
aaronyin <562540603@qq.com>
2024-07-08 22:31:43 +0800

    update

--- kaggle_house/datas.py
+++ kaggle_house/datas.py
@@ -61,23 +61,25 @@ DATA_HUB['kaggle_house_test'] = (  #@save
     DATA_URL + 'kaggle_house_pred_test.csv',
     'fa19780a7b011d9b009e8bff8e99922a8ee2eb90')
 
-train_data = pd.read_csv(download('kaggle_house_train'))
-test_data = pd.read_csv(download('kaggle_house_test'))
+def load_datas(device):
+  train_data = pd.read_csv(download('kaggle_house_train'))
+  test_data = pd.read_csv(download('kaggle_house_test'))
 
-all_features = pd.concat((train_data.iloc[:, 1:-1], test_data.iloc[:, 1:]))
+  all_features = pd.concat((train_data.iloc[:, 1:-1], test_data.iloc[:, 1:]))
 
-# 若无法获得测试数据，则可根据训练数据计算均值和标准差
-numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
-all_features[numeric_features] = all_features[numeric_features].apply(lambda x: (x - x.mean()) / (x.std()))
-# 在标准化数据之后，所有均值消失，因此我们可以将缺失值设置为0
-all_features[numeric_features] = all_features[numeric_features].fillna(0)
+  # 若无法获得测试数据，则可根据训练数据计算均值和标准差
+  numeric_features = all_features.dtypes[all_features.dtypes != 'object'].index
+  all_features[numeric_features] = all_features[numeric_features].apply(lambda x: (x - x.mean()) / (x.std()))
+  # 在标准化数据之后，所有均值消失，因此我们可以将缺失值设置为0
+  all_features[numeric_features] = all_features[numeric_features].fillna(0)
 
-# “Dummy_na=True”将“na”(缺失值)视为有效的特征值，并为其创建指示符特征
-all_features = pd.get_dummies(all_features, dummy_na=True)
+  # “Dummy_na=True”将“na”(缺失值)视为有效的特征值，并为其创建指示符特征
+  all_features = pd.get_dummies(all_features, dummy_na=True)
 
-n_train = train_data.shape[0]
-train_features = torch.tensor(all_features[:n_train].values.astype(float), dtype=torch.float32)
-test_features = torch.tensor(all_features[n_train:].values.astype(float), dtype=torch.float32)
-train_labels = torch.tensor(
-  train_data.SalePrice.values.reshape(-1, 1), dtype=torch.float32
-)
+  n_train = train_data.shape[0]
+  train_features = torch.tensor(all_features[:n_train].values.astype(float), dtype=torch.float32, device=device)
+  test_features = torch.tensor(all_features[n_train:].values.astype(float), dtype=torch.float32, device=device)
+  train_labels = torch.tensor(
+    train_data.SalePrice.values.reshape(-1, 1), dtype=torch.float32, device=device
+  )
+  return train_features, train_labels, test_features
---
+++ kaggle_house/train.py
@@ -0,0 +1,98 @@
+from datas import load_datas
+import torch
+from d2l import torch as d2l
+import numpy as np
+import pandas as pd
+from models import KaggleHouseModel
+
+loss = torch.nn.MSELoss()
+
+def log_rmse(net, features, labels):
+  # 为了在取对数时进一步稳定该值，将小于1的值设置为1
+  clipped_preds = torch.clamp(net(features), 1, float('inf'))
+  rmse = torch.sqrt(loss(torch.log(clipped_preds), torch.log(labels)))
+  # print(rmse)
+  return rmse.item()
+
+def train(net, train_features, train_labels, test_features, test_labels, num_epochs, learning_rate, weight_decay, batch_size, device):
+  train_ls, test_ls = [], []
+  train_iter = d2l.load_array((train_features, train_labels), batch_size)
+  # 这里使用的是Adam优化算法
+  optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate, weight_decay=weight_decay)
+  for epoch in range(num_epochs):
+    for X, y in train_iter:
+      optimizer.zero_grad()
+      # X, y = X.to(device), y.to(device)
+      l = loss(net(X), y)
+      l.backward()
+      optimizer.step()
+    train_ls.append(log_rmse(net, train_features, train_labels))
+    if test_labels is not None:
+      rmse = log_rmse(net, test_features, test_labels)
+      test_ls.append(rmse)
+  return train_ls, test_ls
+
+def k_fold(k, net, X_train, y_train, num_epochs, learning_rate, weigit_decay, batch_size, device):
+  train_l_sum, valid_l_sum = 0, 0
+  for i in range(k):
+    data = get_k_fold_data(k, i, X_train, y_train)
+    train_ls, valid_ls = train(net, *data, num_epochs, learning_rate, weigit_decay, batch_size, device)
+    train_l_sum += train_ls[-1]
+    valid_l_sum += valid_ls[-1]
+    # if i == 0:
+    #   d2l.plot(list(range(1, num_epochs + 1)), [train_ls, valid_ls], xlabel='epoch', ylabel='rmse', xlim=[1, num_epochs], legend=['train', 'valid'], yscale='log')
+    print(f'折{i+1}，训练log rmse{float(train_ls[-1]):f}，'
+          f'验证log rmse{float(valid_ls[-1]):f}，')
+  return train_l_sum / k, valid_l_sum / k
+
+def get_k_fold_data(k, i, X, y):
+  assert k > 1
+  fold_size = X.shape[0] // k
+  X_train, y_train = None, None
+  for j in range(k):
+    idx = slice(j * fold_size, (j + 1) * fold_size)
+    X_part, y_part = X[idx, :], y[idx]
+    if j == i:
+      X_valid, y_valid = X_part, y_part
+    elif X_train is None:
+      X_train, y_train = X_part, y_part
+    else:
+      X_train = torch.cat([X_train, X_part], 0)
+      y_train = torch.cat([y_train, y_part], 0)
+  return X_train, y_train, X_valid, y_valid
+
+def init_weights(m):
+    if type(m) == torch.nn.Linear:
+        torch.nn.init.normal_(m.weight, std=0.01)
+
+if __name__ == "__main__":
+    # print(train_features.shape) # torch.Size([1460, 330])
+    # device = torch.device('mps')
+    device = torch.device('cpu')
+    train_features, train_labels, test_features = load_datas(device)
+
+    in_features = train_features.shape[1]
+    net = KaggleHouseModel(in_features)
+    net.apply(init_weights)
+    net.to(device)
+
+    loss = torch.nn.MSELoss()
+
+    num_epochs, lr, weight_decay, batch_size = 1000, 5, 0, 32
+    k = 5
+
+    timer = d2l.Timer()
+    train_l, valid_l = k_fold(k, net, train_features, train_labels, num_epochs, lr, weight_decay, batch_size, device)
+    print(f'{k}-折验证: 平均训练log rmse: {float(train_l):f}, ', f'平均验证log rmse: {float(valid_l):f}')
+    print(f'{timer.stop():.5f} sec')
+
+    # train_ls, _ = train(net, train_features, train_labels, None, None, num_epochs, lr, weight_decay, batch_size, device)
+    # print(f'{timer.stop():.5f} sec')
+    # d2l.plot(np.arange(1, num_epochs + 1), [train_ls], xlabel='epoch', ylabel='log rmse', xlim=[1, num_epochs], yscale='log')
+    # print(f'训练log rmse:{float(train_ls[-1]):f}')
+    # 将网络应用于测试集。
+    # preds = net(test_features).detach().numpy()
+    # 将其重新格式化以导出到Kaggle
+    # test_data['SalePrice'] = pd.Series(preds.reshape(1, -1)[0])
+    # submission = pd.concat([test_data['Id'], test_data['SalePrice']], axis=1)
+    # submission.to_csv('submission.csv', index=False)
